712c9de0a1045a3e726b1fb98fad017a0f3ab278
Admin <Admin@DESKTOP-9F0GK4C>
2022-12-25 20:30:35 +0530

    Add project files.

---
+++ ExpenseTracker.Api/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using ExpenseTracker.Domain.Modles;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var category = new Category()
+            {
+                CategoryId = id,
+                Icon = "icon",
+                Title= "Title",
+                Type="Test"
+            };
+
+            return Ok(category);
+        }
+    }
+}
---
+++ ExpenseTracker.Domain/Modles/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Domain.Modles
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(256)")]
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
+    }
+}
